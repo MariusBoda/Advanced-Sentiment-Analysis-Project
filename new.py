017157d93fd5f87abef35f4eb53cd102b55b6736
MariusBoda <35667954+MariusBoda@users.noreply.github.com>
2025-01-05 17:56:31 -0800

    writing metaflow flow for new dataset

--- new.py
+++ new.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import spacy
+import kagglehub
 from metaflow import FlowSpec, step, Parameter, current
 
 class RNNModel(nn.Module):
@@ -34,20 +35,31 @@ class RNNModel(nn.Module):
 class SentimentFlow(FlowSpec):
 
     glove_path = Parameter("glove_path", default="glove/glove.6B.100d.txt")
-    kaggle_dataset = Parameter("kaggle_dataset", default="kazanova/sentiment140")
+    kaggle_dataset = Parameter("kaggle_dataset", default="ankurzing/sentiment-analysis-for-financial-news")
     num_epochs = Parameter("num_epochs", 5)
     batch_size = Parameter("batch_size", 64)
     hidden_size = Parameter("hidden_size", 128)
-    output_size = Parameter("output_size", 2) # (positive, negative)
+    output_size = Parameter("output_size", 3) # (positive, negative)
     
     @step
     def start(self):
-        pass
+        print("Downloading dataset...")
+        self.dataset_path = kagglehub.dataset_download(self.kaggle_dataset)
+        self.file_path = os.path.join(self.dataset_path, "all-data.csv")
+        
+        print("Loading GloVe embeddings...")
+        if not os.path.exists(self.glove_path):
+            raise FileNotFoundError(f"GloVe file not found at: {self.glove_path}")
+        else:
+            self.glove_embeddings = self.load_glove_embeddings(self.glove_path)
         self.next(self.load_data)
 
     @step
     def load_data(self):
-        pass      
+        columns = ["Sentiment", "News Headline"]
+        df = pd.read_csv(self.file_path, encoding='latin-1', names=columns)
+        df.rename(columns={"Sentiment": "label", "News Headline": "text"}, inplace=True)     
+           
         self.next(self.preprocess)
 
     @step
